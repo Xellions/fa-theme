9da05963e746915737df3ebe78ebbfb43d294b1b
Carlos Orellana <xellions@gmail.com>
2020-03-07 16:43:03 -0300

    se comienza el proyecto de maqueta plana de ForoAnime

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+/*librerias requeridas para gulp*/
+let gulp 		= require('gulp');
+let gulp_postcss= require('gulp-postcss');
+/*librerias de postcss*/
+let colorFunction 	= require('postcss-color-function');
+let extend			= require('postcss-extend');
+let imports			= require('postcss-import');
+let math 			= require('postcss-math');
+let mixins			= require('postcss-mixins');
+let nested			= require('postcss-nested');
+let varibles		= require('postcss-advanced-variables');
+/*librerias fuera de postcss*/
+let autoprefixer 	= require('autoprefixer');
+/*rutas*/
+let input	= 'postcss/';
+let output	= 'css';
+
+function makeACompilator(){
+	let pluggins = new Array();
+	pluggins =  [
+					imports(),
+					mixins(),
+					varibles(),
+					nested(),
+					extend(),
+					colorFunction({preserveCustomProps:true}),
+					math(),
+					autoprefixer()
+			   ];
+
+	let compilation = gulp.src(input+'*.css')
+							.pipe(gulp_postcss(pluggins))
+							.pipe(gulp.dest(output));
+	
+	return compilation;
+}
+
+gulp.task(
+			'compile', 
+			gulp.series(
+						function(){
+							return makeACompilator();
+						}));
+gulp.task('watch', function() {
+	console.log('-------------------------0-------------------------');
+  	gulp.watch(input+'*.css', gulp.task('compile'));
+  	console.log('-------------------------0-------------------------');
+});
