805ef5e86a31ce44e0769722768e140fa59fe1b4
Carlos Orellana <xellions@gmail.com>
2020-03-10 00:27:31 -0300

    se cambia extension de index para soportar llamados y lenguaje php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es-CL">
+<head>
+	<title>Página de inicio</title>
+	<?php include ('header_calls.php');?>	
+</head>
+<body>
+	<header>
+		<?php
+			include('top_header.php'); 
+		?>
+	</header>
+	<h1>test</h1>
+</body>
+</html>
