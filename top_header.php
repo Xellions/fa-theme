8fb122abecd5d707b35dca29d7536ddc60a03d07
Carlos Orellana <xellions@gmail.com>
2020-03-10 00:30:13 -0300

    menu correspondiente a franja negra en theme hamtaro

---
+++ top_header.php
@@ -0,0 +1,21 @@
+<div class="menu-top">
+    <div class="d-flex menu-container">
+    	<nav>
+	    	<ul class="no-list no-margin d-flex important">
+		        <li><a href="https://foroanime.net/Tema-Reglamento-General-FA">Reglas de convivencia</a></li>
+		        <li><a href="https://foroanime.net/memberlist.php">Miembros</a></li>
+		        <li><a href="https://foroanime.net/games.php">Games</a></li>
+		        <li><a href="https://foroanime.net/misc.php?page=medallasfa">Medallas</a></li>
+		        <li><a href="https://foroanime.net/misc.php?page=Alianzas">Alianzas</a></li>
+		    </ul>
+	    </nav>
+	    <nav>
+	        <ul class="no-list no-margin d-flex social">
+	            <li><a href="https://www.facebook.com/foroanime/"><i class="fab fa-facebook-f"></i></a></li>
+	            <li><a href="https://twitter.com/ForoAnime"><i class="fab fa-twitter"></i></a></li>
+	            <li><a href="https://www.youtube.com/user/ForoAnimenet"><i class="fab fa-youtube"></i></a></li>
+	            <li><a href="https://discord.gg/6PSBcej"><i class="fab fa-discord"></i></a></li>
+	        </ul>
+	    </nav>
+    </div>
+</div>
