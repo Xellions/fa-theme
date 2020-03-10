fa09f2b8e3df78de453605b51a53029fc11461b3
Carlos Orellana <xellions@gmail.com>
2020-03-10 00:29:17 -0300

    archivo que soportará cada uno de los posibles formatos, como testeo

---
+++ visual_guide.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es-CL">
+<head>
+	<title>líneas generales de estilos</title>
+	<?php include ('header_calls.php');?>
+<body>
+	<section class="container">
+		<h1>h1: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h1>
+		<h2>h2: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h2>
+		<h3>h3: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h3>
+		<h4>h4: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h4>
+		<h5>h5: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h5>
+		<h6>h6: José compró una vieja zampoña en Perú. Excusándose, Sofía tiró su whisky al desagüe de la banqueta.</h6>
+		
+		<h2>Ejemplos re texto</h2>
+
+		<p>En un lugar de la Mancha,<a title="example" href="#"> de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero</a>, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los días de entre semana se honraba con su vellori de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, <q>y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza</q>, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años, era de complexión recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosímiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.</p>
+
+		<blockquote>Es, pues, de saber, que este sobredicho hidalgo, los ratos que estaba ocioso (que eran los más del año) se daba a leer libros de caballerías con tanta afición y gusto, que olvidó casi de todo punto el ejercicio de la caza, y aun la administración de su hacienda; y llegó a tanto su curiosidad y desatino en esto, que vendió muchas hanegas de tierra de sembradura, para comprar libros de caballerías en que leer; y así llevó a su casa todos cuantos pudo haber dellos; y de todos ningunos le parecían tan bien como los que compuso el famoso Feliciano de Silva:</blockquote>
+
+		<figure>
+			<img src="https://i.imgur.com/ly5ljM4.jpg" alt="[Imagen: ly5ljM4.jpg]" class="mycode_img">
+			<figcaption>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, quibusdam.</figcaption>
+		</figure>
+	</section>
+	<section class="grid container">
+		<h1>Últimos post:</h1>
+		<article class="card">
+			<figure>
+				<img src="https://i.imgur.com/ly5ljM4.jpg" alt="[Imagen: ly5ljM4.jpg]" class="mycode_img">
+			</figure>
+			<h2>Trailer - Ghost of Tsushima</h2>
+			<p class="description">Hola a tod@s!! Por fin después de un par de años de secretismo por parte de Sony y Sucker punch, ya tenemos un poco mas de noticias sobre (...)</p>
+			<p class="data">
+				<span class="author"><b>@Belmont</b></span>
+				<span class="where">videoJuegos</span>
+			</p>
+		</article>
+	</section>
+</body>
+</html>
