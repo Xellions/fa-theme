bb26b72007f04b137aa3fc5605c8929bb02cd6d4
Carlos Orellana <xellions@gmail.com>
2020-03-10 00:26:59 -0300

    se separan llamados para realizarlos solo desde un archivo

---
+++ header_calls.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/all.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Roboto:300,400,700&display=swap" rel="stylesheet">
+<script>document.write('<script src="http://' + (location.host || 'localhost/themeFA/').split(':')[0] + ':35729/livereload.js?snipver=2"></' + 'script>')</script>
